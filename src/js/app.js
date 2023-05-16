88983cb8a2a04b89c42f448a0cdfc6f10a17e871
JuanSolar <juansolarsolar@yahoo.com>
2023-05-16 18:12:48 +0000

    avance

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,27 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
+// /**
+//  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
+//  *  This function is called every time the user changes types or changes any input
+//  *
+//     {
+//         includeCover: true, // if includeCover is true the algorithm should
+//         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
+//         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+//         socialMediaPosition: "left", // social media bar position (left or right)
 
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
+//         twitter: null, // social media usernames
+//         github: null,
+//         linkedin: null,
+//         instagram: null,
+
+//         name: null,
+//         lastname: null,
+//         role: null,
+//         country: null,
+//         city: null
+//     }
+//  */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
@@ -30,17 +30,18 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+  document.querySelector(
+    "#widget_content"
+  ).innerHTML = `<div class="widget"> ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname} </h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
+          <ul class= ${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${variables.twitter}" ><i class="bi bi-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="bi bi-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="bi bi-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="bi bi-instagram"></i></a></li>
           </ul>
         </div>
     `;
